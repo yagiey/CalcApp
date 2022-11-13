650863088b8802a9b81552a13d6e63cb1cffbf7c
yagiey <yagiey@gmail.com>
2022-11-13 21:38:02 +0900

    Add project files.

---
+++ CalcApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalcApp
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			// See https://aka.ms/new-console-template for more information
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
